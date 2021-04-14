6c2335c3e4a9371d97819d452c79768ee04c0894
bhaskar27101987 <79414130+bhaskar27101987@users.noreply.github.com>
2021-04-14 17:32:03 +0530

    Delete lab1 directory

--- lab1/sp_provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-    features {}
-  subscription_id = "71538297-cbdb-4a31-a6a1-328fa3ce2fe4"
-  client_id = "f68c527c-12a2-457a-9f28-d6114eb4d27d"
-  client_secret = "RRfaDMdj_Ob6GLzTgZTMPyisPJ5.IBeo1x"
-  tenant_id = "48df8adc-e5a3-4873-bd63-f596c43a8e53"
-}
